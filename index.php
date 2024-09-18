c8645899261f7ed2067a61e91963784b8684e04b
vinicius <vinicius.generoso2@etec.sp.gov.br>
2024-09-17 22:19:12 -0300

    corpo da home,imagem de fundo,navbar

--- index.php
+++ index.php
@@ -1,30 +1,22 @@
 <?php include "cabecalho.php"; ?>
+
+
 <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a id="navbar_logo" href="index.php">FitRealTime</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a id="nav-link" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a id="nav-link" href="sobre.php">Sobre</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+          <a id="nav-link" href="perfil.php">Perfil</a>
         </li>
       </ul>
     </div>
@@ -42,22 +34,7 @@
         </div>
         <button type="submit" class="btn btn-primary">Pesquisar</button>
     </form>
-    <!--
-    <h2>Login</h2>z
-    <form action="login_page.php" method="POST">
-        <div>
-            <label for="exampleInputEmail1">Email</label>
-            <input type="email" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" required>
-        </div>
-        <div>
-            <label for="exampleInputPassword1">Senha</label>
-            <input type="password" name="senha" id="exampleInputPassword1" required>
-        </div>
-        <button type="submit">Entrar</button>
-        <a href="cadastro_usuario.php">Cadastrar usuário</a>
-        <a href="cadastro_admin.php">Cadastrar admin</a>
-    </form>
-    -->
-</main>
+</main>  
+  
 <?php include "rodape.php"; ?>
 
